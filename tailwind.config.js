14437baaf3261d9d0eaab159e1dcd09c8b85ddf5
doobeedoobeedam <kwecitra@gmail.com>
2022-07-06 18:07:45 +0700

    finished? i hope :>

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'white': '#ffffff',
+      'primary': '#1D9BF0',
+      'secondary': '#828282',
+      'light': '#F8F8F8',
+      'dark': '#141D26',
+    },
+    fontFamily: {
+      
+    }
+  },
+  plugins: [],
+}
