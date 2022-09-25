edf6cee2d83b52e79e973648235f68c09455f483
doobeedoobeedam <kwecitra@gmail.com>
2022-09-25 19:02:08 +0700

    dark theme

--- dist/js/main.js
+++ dist/js/main.js
@@ -48,9 +48,9 @@ function updateDialog(project) {
 
         <div class="flex justify-end pt-3 border-t border-t-light dark:border-t-secondary [&>a]:self-center [&>a>svg]:w-6 [&>a>svg]:h-6 [&>a>svg]:text-secondary [&>a>svg]:mr-3 [&>button]:px-4 [&>button]:py-2 [&>button]:font-semibold [&>button]:bg-primary [&>button]:text-white [&>button]:rounded-md [&>button]:cursor-pointer">
 
-            ${project.github ? `<a href="${project.github}"><svg class="hover:text-primary transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path></svg></a>` : ``}
+            ${project.github ? `<a href="${project.github}"><svg class="dark:text-soft hover:text-primary transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path></svg></a>` : ``}
 
-            ${project.url ? `<a href="${project.url}"><svg class="hover:text-primary transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"></path></svg></a>` : ``}
+            ${project.url ? `<a href="${project.url}"><svg class="dark:text-soft hover:text-primary transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"></path></svg></a>` : ``}
             
             <button id="close" class="text-sm lg:text-base">Close</button>
         </div>
@@ -60,13 +60,11 @@ function updateDialog(project) {
 function showProjects(projects) {
     return `
         ${projects.map((project, i) => `
-            <div data-index="${i}" class="relative cursor-pointer open project mix ${project.category} overflow-hidden rounded-md border border-light dark:border-0 dark:bg-soft-dark">
-                <div class="mb-3 lg:mb-0">
-                        <img src="dist/img/portfolios/${project.image}" alt="${project.title}">
-                        <div class="px-3 py-3">
-                            <h1 class="font-semibold text-dark dark:text-soft tracking-wide">${project.title}</h1>
-                            <span class="text-sm text-primary block">${project.tag}</span>
-                        </div>
+            <div data-index="${i}" class="relative cursor-pointer open project mix ${project.category} overflow-hidden rounded-md border border-light dark:border-0 dark:bg-soft-dark mb-3 lg:mb-0">
+                <img src="dist/img/portfolios/${project.image}" alt="${project.title}">
+                <div class="px-3 py-3">
+                    <h1 class="font-semibold text-dark dark:text-soft tracking-wide">${project.title}</h1>
+                    <span class="text-sm text-primary block">${project.tag}</span>
                 </div>
             </div>
         `).join("")}`
